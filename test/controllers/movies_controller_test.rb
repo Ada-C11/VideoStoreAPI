1adbefffd78046fa67d14c80b92ddc69234c6480
sjscotton <sj.scotton@gmail.com>
2019-05-14 13:35:20 -0700

    added edge case test for movies#show

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -7,5 +7,12 @@ describe MoviesController do
       get movie_path(movie.id)
       must_respond_with :success
     end
+
+    it "will respond with not found of given an invalid movie id" do
+      fake_id = -1
+      get movie_path(fake_id)
+
+      must_respond_with :not_found
+    end
   end
 end
