1adc8fb30750dbd5d00d6a1028896501fafeea29
Kate Sandler <kate@katekatekate.net>
2019-05-14 14:06:42 -0700

    Tests for rental model

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -4,12 +4,11 @@ describe Rental do
   let(:rental) { rentals(:rentalone) }
 
   describe "validations" do
-
-    it 'is valid if all fields are present' do
+    it "is valid if all fields are present" do
       expect(rental).must_be :valid?
     end
 
-    it 'is not valid if it does not have a movie' do
+    it "is not valid if it does not have a movie" do
       expect(rental).must_be :valid?
 
       rental.movie = nil
@@ -17,7 +16,7 @@ describe Rental do
       expect(rental).wont_be :valid?
     end
 
-    it 'is not valid if it does not have a customer' do
+    it "is not valid if it does not have a customer" do
       expect(rental).must_be :valid?
 
       rental.customer = nil
