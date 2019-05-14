69185eb2368a1c959ba43b0b99f1d77db6653c11
Angela <angela.ohh@gmail.com>
2019-05-14 13:53:42 -0700

    passing index tests in customer#index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,8 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    render json: { ok: true, customer: customers.as_json(only: [:id, :name, :register_at, :postal_code, :phone]) },
+    # render json: { ok: true, customer: customers.as_json(only: [:id, :name, :register_at, :postal_code, :phone]) },
+    render json: customers.as_json(only: [:id, :name, :phone, :postal_code, :registered_at]),
            status: :ok
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    # These tests are a little verbose - yours do not need to be
+    # this explicit.
+    it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get customers_path
+      expect(response.header["Content-Type"]).must_include "json"
+    end
+
+    it "returns an Array" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the pets" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns pets with exactly the required fields" do
+      keys = %w(id name phone postal_code registered_at)
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
+  end
 end
