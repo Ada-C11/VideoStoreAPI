ed5bf84d0f8a1dc4ee26aa4df1c2d258efd1e653
K <774162+kaseea@users.noreply.github.com>
2019-05-14 14:26:40 -0700

    update movie test

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { movies(:test) }
 
   it "must be valid" do
-    value(movie).must_be :valid?
+    expect(movie.valid?).must_equal true
+  end
+
+  it "wont be valid without a title" do
+    movie.title = ""
+    expect(movie.valid?).must_equal false
+  end
+
+  it "wont be valid without inventory" do
+    movie.inventory = ""
+    expect(movie.valid?).must_equal false
+  end
+
+  it "canhaz an associated customer" do
+    expect(movie).must_respond_to :customers
+    expect(movie.customers).must_include customers(:shelley)
+  end
+
+  it "can have an associated rental" do
+    expect(movie).must_respond_to :rentals
+    expect(movie.rentals).must_include rentals(:one)
   end
 end
