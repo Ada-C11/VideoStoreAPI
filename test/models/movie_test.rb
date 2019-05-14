44953a690b8084e1dce36dc8b56d5b25bacdcc97
Ariana Bray <aribray@gmail.com>
2019-05-14 14:41:23 -0700

    added validation tests to movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -6,4 +6,35 @@ describe Movie do
   it "must be valid" do
     value(movie).must_be :valid?
   end
+
+  describe "validations" do
+
+    it "must have a title" do
+      movie.title = nil
+      movie.save
+      expect(movie.valid?).must_equal false
+      expect(movie.errors).must_include "title"
+    end
+
+    it "must have an overview" do
+      movie.overview = nil
+      movie.save
+      expect(movie.valid?).must_equal false
+      expect(movie.errors).must_include "overview"
+    end
+
+    it "must have a release date" do
+      movie.release_date = nil
+      movie.save
+      expect(movie.valid?).must_equal false
+      expect(movie.errors).must_include "release_date"
+    end
+
+    it "inventory must be a number" do
+      movie.inventory = "abcd"
+      movie.save
+      expect(movie.valid?).must_equal false
+      expect(movie.errors[:inventory].first).must_include "is not a number"
+    end
+  end
 end
