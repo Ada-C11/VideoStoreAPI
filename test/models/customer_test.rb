c52cc6c5651cd293c347cad398db3a08e96711df
Sophearychiv <sopheary.chiv@gmail.com>
2019-05-14 16:42:35 -0700

    tests for customer model

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,28 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { customers(:sophie) }
+  let(:movie) { movies(:one) }
+  describe "validations" do
+    it "must be valid" do
+      expect(customer.valid?).must_equal true
+    end
+  end
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "relationships" do
+    it "can have 0 rentals" do
+      customer.rentals
+      expect(customer.rentals.count).must_equal 0
+    end
+
+    it "can have 1 or more rentals" do
+      rental = Rental.create(customer_id: customer.id,
+                             movie_id: movie.id,
+                             checkout_date: Date.today,
+                             due_date: Date.today + 7)
+
+      customer.rentals << rental
+      expect(customer.rentals).must_include rental
+    end
   end
 end
