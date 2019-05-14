96453aa352d62991f659f3182c3285f20ec5add6
Nidhi <nidhixpatel@gmail.com>
2019-05-14 14:37:18 -0700

    added get customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,10 @@
 class CustomersController < ApplicationController
+  
   def index
+    customers = Customer.all
+    render json:  customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
   end
+
+
+  
 end
