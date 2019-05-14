b43ccf3c2e616fda4ad9ad997259a4f01f2e0f11
Angela <angela.ohh@gmail.com>
2019-05-14 13:25:18 -0700

    created model for movie and customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get "/movies", to: movies#index
-  # get "/movies/:id", to: movies#show
-  # post "/movies", to: movies#create
-  # post "/movies/:id", to: movies#update
-  # get "/movies/:id, to: movies#destroy
+  # resources: :movies, only: [:index, :show, :create]
+  ### get "/movies", to: movies#index
+  ### get "/movies/:id", to: movies#show
+  ### post "/movies", to: movies#create
 
   # get "/customers", to: customers#index
-  # get "/customers/:id", to: customers#show
-  # post "/customers", to: customers#create
-  # post "/customers/:id", to: customers#update
-  # get "/customers/:id, to: customers#destroy
+
+  # Which controller do these routes route to?
+  # post "/rentals/check-out", to:
+  # post "/rentals/check-in", to:
 
 end
