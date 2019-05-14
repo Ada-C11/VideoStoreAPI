da5ac64b0921bd850e7fbccf701d3dd345c8b836
amyesh <amyesh08@gmail.com>
2019-05-14 15:00:52 -0700

    added validation tests for all models

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,14 +1,10 @@
 require "test_helper"
 
 describe Customer do
+  let (:customer) { customers(:one) }
   before do
     @customer = Customer.new(
       name: "test_name",
-      phone: "132-456-7890",
-      registered_at: DateTime.now,
-    )
-    @invalid_customer = Customer.new(
-      name: "",
       phone: "132-456-7890",
       registered_at: DateTime.now,
     )
@@ -17,8 +13,14 @@ describe Customer do
   it "must be valid given good data" do
     expect(@customer).must_be :valid?
   end
-  it "will throw an error if given invalid data" do
-    result = @invalid_customer.valid?
-    expect(result).must_equal false
+
+  it "requires name, phone, registered_at" do
+    required_fields = [:name, :phone, :registered_at]
+
+    required_fields.each do |field|
+      customer[field] = nil
+      expect(customer.valid?).must_equal false
+      customer.reload
+    end
   end
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  let (:rental) { rentals(:one) }
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+  it "must be valid given good data" do
+    expect(rental.valid?).must_equal true
+  end
+
+  it "requires due_date" do
+    required_fields = [:due_date]
+
+    rental.due_date = nil
+    expect(rental.valid?).must_equal false
+    rental.reload
   end
 end
