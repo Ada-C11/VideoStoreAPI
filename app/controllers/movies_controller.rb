ca02b88952a88b37e5c7b82619eb98737e1e983d
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-14 15:38:58 -0700

    Add movie create action & strong params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,20 @@ class MoviesController < ApplicationController
 
   def show
   end
+
+  def create
+    movie = Movie.new(movie_params)
+    if movie.save
+      render json: { id: movie.id }, status: :ok
+    else
+      render json: { errors: movie.errors.messages },
+        status: :bad_request
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:id, :title, :release_date)
+  end
 end
