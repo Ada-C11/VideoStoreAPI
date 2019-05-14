0fbef4cb4005453310b973714b3a5c64cdcbaa93
Kate Sandler <kate@katekatekate.net>
2019-05-14 14:44:45 -0700

    Controller for customers

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,13 @@
+class CustomersController < ApplicationController
+  def index
+    sort_options = ["name", "registered_at", "postal_code"]
+    list = Customer.all
+    @customers = sort_and_paginate(sort_options, list, params)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone, :sort, :p, :n)
+  end
+end
