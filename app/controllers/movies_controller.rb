80804e6d96c070759c553b438c3e45c9cdd328ea
laneia <laneiamurray@gmail.com>
2019-05-14 16:03:24 -0700

    added movies index method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
   def zomg
-    render json: { ready_for_lunch: "It works" }
+    render json: {ready_for_lunch: "It works"}
+  end
+
+  def index
+    params[:sort] ? movies = Movie.order(params[:sort]) : movies = Movie.all
+
+    render json: movies.as_json, status: :ok
   end
 end
