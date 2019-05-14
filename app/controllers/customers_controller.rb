221d3c52917a0601ea309826c06abc694e42708c
Ngoc Le <ngocle@outlook.com>
2019-05-14 14:17:02 -0700

    added test for customers controller - index method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
+    #once we added foreign key movie checkout count, we will need to update the controller test
     render status: :ok, json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
   end
 end
