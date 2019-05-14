f871a4cfb398653935c73db71ac0882c680a5daf
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-14 15:42:39 -0700

    rental tests

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "checkin" do
+    before do
+      @rental = rentals(:one)
+      @rental_params = { movie_id: @rental.movie_id, customer_id: @rental.customer_id }
+      @customer = Customer.find_by(id: @rental.customer_id)
+      @movie = Movie.find_by(id: @rental.movie_id)
+    end
+
+    it "decrements customer's checked_out_count" do
+      post checkin_path, params: @rental_params
+
+      expect
+    end
+  end
 end
