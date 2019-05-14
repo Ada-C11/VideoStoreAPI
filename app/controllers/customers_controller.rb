c3455a1f788a0e1fe6bf5325a88af770d26549cb
Riyo <riyo.perry@gmail.com>
2019-05-14 15:04:42 -0700

    customers controller and tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,12 @@
 class CustomersController < ApplicationController
-    def zomg
-        render json: {text: "It works"}
+    def index
+        customers = Customer.all
+        render status: :ok, json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
+    end
+
+
+    private
+    def customer_params
+      params.require(:customer).permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone)
     end
 end
