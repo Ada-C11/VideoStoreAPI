62aaff36b4afd17d4796c04b655fa0a15bec27b8
laneia <laneiamurray@gmail.com>
2019-05-14 14:19:39 -0700

    added create valid rental test

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "creating rental" do
+    let(:rental_data) {
+      {
+        movie_id: movies(:one).movie_id,
+        customer_id: customers(:one).id,
+      }
+    }
+
+    it "can create a new rental provided valid data" do
+      expect {
+        post rental_path, params: rental_data
+      }.must_change "Rental.count", +1
+    end
+  end
 end
