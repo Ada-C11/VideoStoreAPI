57bf2a1435f0d4b8a3aa1c58a2bc9bc2a8ec24af
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-14 14:16:05 -0700

    merging to get validations

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -13,5 +13,12 @@ describe CustomersController do
       get customer_path(Customer.first)
       value(response).must_be :successful?
     end
+
+    it "returns an error when customer ID is invalid" do
+      get customer_path(Customer.last.id + 1)
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body["errors"]).must_be_kind_of Array
+    end
   end
 end
