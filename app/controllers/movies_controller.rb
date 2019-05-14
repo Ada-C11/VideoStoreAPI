f2effd389c1b7f36d5b300a910f324fc5b3beac2
Karla Guadron <ksguadron@gmail.com>
2019-05-14 14:17:28 -0700

    Wrote show action and tests for movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,10 @@
 class MoviesController < ApplicationController
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory]), status: :ok
+    else
+      render status: :not_found, json: { "errors": movie.errors.messages }
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "show" do
+    it "can show data for a movie that exists" do
+      get movie_path(Movie.first.id)
+      must_respond_with :success
+    end
+
+    it "returns 404 for a movie that doesn't exist" do
+      get movie_path(-1)
+      must_respond_with :error
+    end
+  end
 end
