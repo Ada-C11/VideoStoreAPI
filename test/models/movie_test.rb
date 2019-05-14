904cb5423ebad0fc12b193d465333b9fedf96c0e
Stephanie Marchante <smarcha04@gmail.com>
2019-05-14 14:58:01 -0700

    added relationship and validation tests for models

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -12,11 +12,22 @@ describe Movie do
 
   describe "validations" do
     it "valid movie has a title" do
-      
+      expect(movie).must_be :valid?
     end
 
     it "invalid movie does not have a title" do
+      movie.title = nil
+      expect(movie).wont_be :valid?
+    end
 
+    it "invalid movie has no inventory" do
+      movie.inventory = nil
+      expect(movie).wont_be :valid?
+    end
+
+    it "invalid if inventory is a negative number" do
+      movie.inventory = -10
+      expect(movie).wont_be :valid?
     end
   end
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  let(:rental) { rentals(:one) }
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+
+  describe "relationships" do
+    it "has a customer" do
+      expect(rental.customer).must_be_instance_of Customer
+    end
+
+    it "has a movie" do
+      expect(rental.movie).must_be_instance_of Movie
+    end 
   end
 end
