863f766f390dda1333fc0127777642474d8a5fe0
qqdipps <sdipp007@gmail.com>
2019-05-14 15:05:26 -0700

    added migration to include available_inventory on movies, updated seed file for defualt to inventory count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_192647) do
+ActiveRecord::Schema.define(version: 2019_05_14_220129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_192647) do
     t.integer "inventory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "available_inventory"
   end
 
   create_table "rentals", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,7 @@ JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
 end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
-  Movie.create!(movie)
+  movie_new = Movie.create!(movie)
+  movie_new.update(available_inventory: movie_new.inventory)
+
 end
