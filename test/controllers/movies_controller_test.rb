56e0ba22e0f0067833ce4b84638e5e2246adedf9
laneia <laneiamurray@gmail.com>
2019-05-14 15:09:11 -0700

    added check for json and array in index test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -2,9 +2,18 @@ require "test_helper"
 
 describe MoviesController do
   describe "index" do
-    it "is a working route" do
+    it "is a working route that returns a JSON array" do
       get movies_path
+
       must_respond_with :success
+      expect(response.header["Content-Type"]).must_include "json"
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
     end
+
+    # it "" do
+
+    # end
   end
 end
