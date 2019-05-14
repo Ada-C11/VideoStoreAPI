737a3efb33f9b42d7fba187995707b69aa786d4e
Margaret Finnan <msfinnan@gmail.com>
2019-05-14 15:04:45 -0700

    finished controller tests and actions for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,26 @@ class MoviesController < ApplicationController
     render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
 
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie #if movie is not nil
+      render json: movie.as_json(only: [:id, :inventory, :overview, :title, :release_date]), status: :ok
+    else
+      render json: {ok: false, errors: "Movie not found"}, status: :not_found
+    end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save #is successful
+      render json: movie.as_json(only: [:id, :overview, :title, :release_date]), status: :ok #include id so they can find pet later if they want to
+    else
+      render json: {ok: false, errors: movie.errors.messages}, status: :bad_request
+    end
+  end
+
   private
 
   def movie_params
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -26,13 +26,54 @@ describe MoviesController do
       body.length.must_equal Movie.count
     end
 
-    it "returns pets with exactly the required fields" do
+    it "returns movies with exactly the required fields" do
       keys = %w(id release_date title)
       get movies_path
       body = JSON.parse(response.body)
       body.each do |movie|
         movie.keys.sort.must_equal keys
       end
+    end
+  end
+
+  describe "show" do
+    it "can get a movie" do
+      get movie_path(movies(:movie_one).id)
+      must_respond_with :success
+    end
+
+    it "responds with not found and errors hash if movie is not found" do
+      get movie_path(-1)
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      body["ok"].must_equal false
+      body["errors"].must_equal "Movie not found"
+    end
+  end
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "Titanic",
+        overview: "Romantic & sad",
+        release_date: 1997 - 12 - 19,
+        inventory: 10,
+      }
+    }
+
+    it "creates a new movie given valid data" do
+      expect {
+        post movies_path, params: {movie: movie_data}
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+
+      expect(movie.title).must_equal movie_data[:title]
+      must_respond_with :success
     end
   end
 end
