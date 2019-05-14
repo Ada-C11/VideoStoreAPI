f28ecfb1b9b9d5c80a74b5e79c0db1426222b606
K <774162+kaseea@users.noreply.github.com>
2019-05-14 11:45:50 -0700

    add routes for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :customers, only: [:index]
+
+  resources :movies, only: [:index, :show, :create]
+
+  post "rentals/check-out", as: "checkout"
+  post "rentals/check-in", as: "checkin"
 end
