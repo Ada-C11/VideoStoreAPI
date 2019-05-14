1bd1de10523fb6fe0c3c47f25e1b2beed654ca84
Ngoc Le <ngocle@outlook.com>
2019-05-14 16:11:58 -0700

    added checkout_inventory custom method in the customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,11 @@
 class Customer < ApplicationRecord
-<<<<<<< HEAD
-  has_many :rentals
-=======
   validates :name, presence: true
->>>>>>> 2be0cb23286d5e65bfb05b09caad9c1a7cf03a39
+
+  def self.checkout_inventory(movie)
+    if movie.available_inventory > 1
+      movie.available_inventory -= 1
+    else
+      return false
+    end
+  end
 end
