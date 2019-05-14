023993e8bc7638e3d84f534db98fcb359a9f7c02
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-14 15:05:08 -0700

    Add movies_checked_out_count column to Customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
-  def zomg
-    zomg = ["it works!"]
-    render json:zomg
+  def index
+    customers = Customer.all
+    render json:  customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok
   end
 end
