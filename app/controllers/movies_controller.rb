8ec064bb7d2c7556bcea795b0bfaa615e725a557
sjscotton <sj.scotton@gmail.com>
2019-05-14 14:12:32 -0700

    added movies#create with tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,9 +3,26 @@ class MoviesController < ApplicationController
     id = params[:id]
     movie = Movie.find_by(id: id)
     if movie
-      render json: movie.as_json(only: [:id, :title, :overview, :release_date]), status: :ok
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
     else
       render json: { errors: ["The movie with id #{id} was not found"] }, status: :not_found
     end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: { id: movie.id }, status: :ok
+    else
+      render json: { errors: movie.errors.messages },
+             status: :bad_request
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
   end
 end
