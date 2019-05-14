e6ff92450d0c64706156a37f1c059df53056b32f
Elise Pham <elise.pham88@gmail.com>
2019-05-14 14:14:34 -0700

    added index action and tests for movies controller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is a working route" do
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "returns an Array" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(id title release_date)
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.must_equal keys
+      end
+    end
+  end
 end
