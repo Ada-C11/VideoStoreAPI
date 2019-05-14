0999c6eda073fd60c9dc061df45ce623fc7daf5b
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-14 15:34:07 -0700

    Test Customers Controller Index

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  describe "index" do
+    it "is a real working route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get customers_path
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+
+    it "returns an array" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all the customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with exactly the required fields" do
+      keys = %w(id name registered_at postal_code phone movies_checked_out_count)
+      
+      get customers_path
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys.sort
+      end
+    end
+  end
+
+
+  # describe "show" do
+  
   # end
+
+  # describe "create" do
+  #   it "creates a new customer given valid data" do
+
+  #   end
+
+  #   it "returns an error for invalid customer data" do
+
+  #   end
+  # end
+
 end
