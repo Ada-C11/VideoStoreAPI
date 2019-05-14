fe324d00e62bd95d38abaaa060dbe60668a1f6af
Angela <angela.ohh@gmail.com>
2019-05-14 14:16:34 -0700

    added test for movie#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie.as_json(only: [:id, :inventory, :overview, :release_date, :title]),
+             status: :ok
+    else
+      render json: { ok: false, errors: { movie: ["Movie not found"] } },
+             status: :not_found
+    end
+  end
 end
