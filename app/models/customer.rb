8a16b20eb7a25935ed6f2ab1ce75beb888c2609b
K <774162+kaseea@users.noreply.github.com>
2019-05-14 13:54:47 -0700

    add validation for models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   has_many :movies, through: :rentals
+
+  validates :name, :inventory, presence: true
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,6 @@
 class Rental < ApplicationRecord
   belongs_to :movie
   belongs_to :customer
+
+  validates :checkout, :due, presence: true
 end
