1cc81c11314b4226e787bdf6f6d5ad5186e8fc61
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-14 13:01:00 -0700

    available_inventory for movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_185107) do
+ActiveRecord::Schema.define(version: 2019_05_14_195802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_185107) do
     t.string "overview"
     t.date "release_date"
     t.integer "inventory"
+    t.integer "available_inventory"
   end
 
 end
