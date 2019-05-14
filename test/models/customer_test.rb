e49232881c714e88480ebcb031150551e5913f8e
Shubha Rajan <shubharajan94@gmail.com>
2019-05-14 14:19:28 -0700

    passing validation and relation tests for customer

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,26 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { customers(:shelley) }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "validations" do
+    it "must be valid" do
+      value(customer).must_be :valid?
+    end
+
+    it "won't be valid without a name" do
+      customer.name = nil
+      value(customer).wont_be :valid?
+    end
+  end
+  describe "relations" do
+    it "has associated rentals" do
+      expect(customer.rentals).must_include rentals(:one)
+      expect(customer.rentals).must_include rentals(:three)
+    end
+    it "has associated movies" do
+      expect(customer.movies).must_include movies(:test)
+      expect(customer.movies).must_include movies(:other_test)
+    end
   end
 end
