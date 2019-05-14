932e23fa7c09a5b777b04517655d9aef02d2ecc0
laneia <laneiamurray@gmail.com>
2019-05-14 15:04:52 -0700

    added movies index test 1

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is a working route" do
+      get movies_path
+      must_respond_with :success
+    end
+  end
 end
