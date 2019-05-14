c86bf8bce396cdbf930147a8ce5535eb9243e881
Ngoc Le <ngocle@outlook.com>
2019-05-14 14:09:28 -0700

    added index methods for movies controller and customers controller, added testing for movie controller - index method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,6 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    render status: :ok, json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
+  end
 end
