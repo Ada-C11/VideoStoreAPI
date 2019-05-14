b5e75a172c035921b5b7288e96ba75a03d19bcfd
Ariana Bray <aribray@gmail.com>
2019-05-14 12:05:59 -0700

    added customers and movies controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/create'
+  get '/zomg', to: 'movies#zomg'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
