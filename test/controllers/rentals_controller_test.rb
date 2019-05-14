a6096ff41a2796bf5337cb5e2509d7ce17f79298
Ariana Bray <aribray@gmail.com>
2019-05-14 15:45:50 -0700

    updated customers yml to add movies_checked_out column

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,8 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 require 'pry'
 
 describe RentalsController do
-  describe "checkout" do
+  describe 'checkout' do
     let(:rental_data) do
       { rental:
         {
@@ -10,16 +12,22 @@ describe RentalsController do
           movie_id: movies(:one).id
         } }
     end
-    it "must create a new rental" do
-      expect {
+    it 'must create a new rental' do
+      expect do
         post checkout_path, params: rental_data
-      }.must_change "Rental.count"
+      end.must_change 'Rental.count'
     end
 
-    it "must change the available_inventory of the checkout out movie" do
-      
+    it 'must change the available_inventory of the checked out movie' do
+      count = movies(:one).available_inventory
+
+      post checkout_path, params: rental_data
+
+      expect(movies(:one).reload.available_inventory).must_equal count - 1
     end
 
-
+    it 'must change the movies_checked_out count of the customer' do
+      count = customers(:joe).movies_checked_out
+    end
   end
 end
