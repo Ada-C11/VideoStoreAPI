986893cd48461cf75f66da594817562b6ee82c78
Hana <hanalways@gmail.com>
2019-05-14 16:22:31 -0700

    passing returns all the customers test

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -30,12 +30,12 @@ describe CustomersController do
       body.must_be_kind_of Array
     end
 
-    # it "returns all of the pets" do
-    #   get pets_path
+    it "returns all of the customers" do
+      get customers_path
 
-    #   body = JSON.parse(response.body)
-    #   body.length.must_equal Pet.count
-    # end
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
 
     # it "returns pets with exactly the required fields" do
     #   keys = %w(age human id name)
