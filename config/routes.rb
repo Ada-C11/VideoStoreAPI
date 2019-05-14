44ae36f46bd019391fb7c6b269a3bb029bb225dd
Nara <naraa_520@yahoo.com>
2019-05-14 14:05:52 -0700

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :movies, only:[:index, :show, :create]
+  resources :customer, only:[:index, :show, :create]
+  post 'rentals/check-out', to: 'rentals#check-out', as:'check-out'
+  post 'rentals/check-in', to: 'rentals#check-in', as:'check-in'
 end
