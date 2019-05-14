b5e776db4c431ba3dd55885b610d3ef5e0ab260d
qqdipps <sdipp007@gmail.com>
2019-05-14 13:50:07 -0700

    yml testing

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { movies(:movie_1) }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "must be valid given complete validations" do
+    expect(movie.valid?).must_be true
+  end
+
+  describe "validations" do
+    it "will not be valid if missing title" do
+      # movie.title = nil
+      expect(movie.valid?).must_be false
+    end
+
+    it "will not be valid if missing overview" do
+    end
+
+    it "will not be valid if missing release_date" do
+    end
+
+    it "will not be valid if missing inventory" do
+    end
   end
 end
+
+# has_many :rentals
+# has_many :customers, through: :rentals
+
+# validates :title, presence: true
+# validates :overview, presence: true
+# validates :release_date, presence: true
+# validates :inventory, presence: true
