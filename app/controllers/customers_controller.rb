9d0041739f43ea0d5f7d49aef28a21a2ca50612c
Jansen <v.jansen.martin@gmail.com>
2019-05-14 16:42:48 -0700

    Add available_inventory column to Movies; Set default to match value in inventory

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,6 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all
 
-    render status: :ok, json: customers.as_json(only: [:name, :registered_at, :phone, :id])
+    render status: :ok, json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
+  before_create :set_available_inventory_default
+
   validates :title, presence: true
+
+  private
+
+  def set_available_inventory_default
+    self.available_inventory = self.inventory
+  end
 end
