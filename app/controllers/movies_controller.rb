87d88e22550c39a5c716a674401cd75cad219efe
Hana <hanalways@gmail.com>
2019-05-14 15:43:22 -0700

    create index action for customers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index 
-    @movies = Movie.all 
+    movies = Movie.all 
+    render status: :ok, json: movies.as_json(only: [:title, :overview, :release_date, :inventory])
   end
 
   def show 
