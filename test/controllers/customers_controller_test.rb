0864e23b9dbee2669b6d023e8500c43a5552ef8f
Nidhi <nidhixpatel@gmail.com>
2019-05-14 15:27:58 -0700

    added movies checked out

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -2,8 +2,17 @@ require "test_helper"
 
 describe CustomersController do
   it "should get index" do
-    get customers_index_url
+    get customers_path
     value(response).must_be :success?
   end
+
+  it "returns customers with exactly the required fields" do
+    keys = %w(address city id name phone postal_code registered_at state)
+    get customers_path
+    body = JSON.parse(response.body)
+    body.each do |customer|
+      customer.keys.sort.must_equal keys
+    end
+  end 
 
 end
