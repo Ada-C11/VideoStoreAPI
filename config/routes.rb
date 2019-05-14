b02a716c1bcf403735cfcf29e75c4db9e3a57eba
Angela <angela.ohh@gmail.com>
2019-05-14 13:58:03 -0700

    created movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources: :movies, only: [:index, :show, :create]
+  resources :movies, only: [:index, :show, :create]
   ### get "/movies", to: movies#index
   ### get "/movies/:id", to: movies#show
   ### post "/movies", to: movies#create
