2c5c257368e6a523d1cbead7706d01ef3640b3f7
laneia <laneiamurray@gmail.com>
2019-05-14 16:17:26 -0700

    added movie show method and params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,27 @@ class MoviesController < ApplicationController
 
     render json: movies.as_json, status: :ok
   end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie.nil?
+      render json: {
+        errors: "Movie not found.",
+      }, status: :not_found
+    else
+      render json: {
+        title: movie.title,
+        overview: movie.overview,
+        release_date: movie.release_date,
+        inventory: movie.inventory,
+      }, status: :ok
+    end
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :overview, :release_date, :inventory)
+  end
 end
