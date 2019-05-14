23623819ba3dfc0b3b4d2d6eafdd6dba063e79a1
Chantal Demissie <chantaldemissie@yahoo.com>
2019-05-14 15:59:16 -0700

    made test for customer index action

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "returns a list of customers" do
+      get customers_path
+      must_respond_with :success
+    end
+  end
 end
