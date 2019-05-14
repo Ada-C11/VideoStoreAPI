953d83fd9f06226877525e29dc78f7885cb0ebad
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-14 14:50:13 -0700

    moved index from model to customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,10 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    if !customers.empty?
+      render json: customers.as_json(except: [:created_at, :updated_at]), status: :ok
+    else
+      render json: { message: "There are currently no customers." }
+    end
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,2 @@
 class Customer < ApplicationRecord
-  def index
-    customers = Customer.all
-    if !customers.empty?
-      render json: customers.as_json(except: [:created_at, :updated_at]), status: :ok
-    else
-      render json: { message: "There are currently no customers." }
-    end
-  end
 end
