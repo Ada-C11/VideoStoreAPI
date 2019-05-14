4322d545faa33bd7cbe9cc16643c2b36fcd558f2
Shubha Rajan <shubharajan94@gmail.com>
2019-05-14 14:55:44 -0700

    added custom methods/validations to check availability of movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,10 @@ class Movie < ApplicationRecord
   has_many :customers, through: :rentals
 
   validates :title, :inventory, presence: true
+
+  def number_available
+    checked_out = self.rentals.select { |rental| rental.due > DateTime.now }.count
+
+    return inventory - checked_out
+  end
 end
