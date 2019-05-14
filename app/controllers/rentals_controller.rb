4b55883ee9e9d4437df6bc90a9af92834fe5cccf
Kate Sandler <kate@katekatekate.net>
2019-05-14 16:03:09 -0700

    Adds checkout custom method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,32 @@
 class RentalsController < ApplicationController
+  def checkout
+    rental = Rental.new(rental_params)
+    customer = find_customer
+    movie = find_movie
+
+    if movie.available_inventory > 0
+      rental.due_date = Date.today + 7
+      if rental.save
+        render json: { rental_id: rental.id }, status: :ok
+      else
+        render_error(:bad_request, rental.errors.messages)
+      end
+    else
+      render_error(:forbidden, "No movie in available inventory.")
+    end
+  end
+
+  private
+
+  def rental_params
+    params.permit(:customer_id, :movie_id)
+  end
+
+  def find_customer
+    return Customer.find_by(id: rental_params[:customer_id])
+  end
+
+  def find_movie
+    return Movie.find_by(id: rental_params[:movie_id])
+  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,15 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+
+  validates :customer_id, presence: true
+  validates :movie_id, presence: true
+
+  # def rent_movie(customer, movie)
+  #   checked_out = customer.movies_checked_out_count
+  #   customer.update(movies_checked_out_count: checked_out +1)
+
+  #   inventory = movie.available_inventory
+  #   movie.update(available_inventory: inventory - 1)
+  # end
 end
