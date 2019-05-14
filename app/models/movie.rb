3f69f9b004acef6708a6db07fbe5b749f457975e
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-14 16:13:17 -0700

    added testing for create action in movies controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   has_many :rentals
+
+  validates :title, presence: true
+  # validates :overview, presence: true
+  # validates :release_date, presence: true
+  validates :inventory, presence: true, numericality: true
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -41,8 +41,47 @@ describe MoviesController do
   #   value(response).must_be :success?
   # end
 
-  # it "should get create" do
-  #   get movies_create_url
-  #   value(response).must_be :success?
-  # end
+  describe "create" do
+    let(:movie_params) {
+      {
+        movie: {
+          title: "Thing",
+          overview: "Test",
+          release_date: "2019-01-01",
+          inventory: 0,
+        },
+      }
+    }
+
+    it "should create a new movie given valid data" do
+      expect {
+        post movies_path(movie_params)
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+      expect(movie.title).must_equal movie_params[:movie][:title]
+      must_respond_with :success
+    end
+
+    it "returns an error for invalid movie data" do
+      movie_params[:movie][:title] = nil
+
+      expect {
+        post movies_path(movie_params)
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      expect(body["errors"]["title"]).must_equal ["can't be blank"]
+      must_respond_with :bad_request
+    end
+  end
 end
