a2f1fc186bdd95b405b296e740851b2c5d5eb250
Margaret Finnan <msfinnan@gmail.com>
2019-05-14 13:28:48 -0700

    fixed due date for rentals fixtures

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  describe "relations" do
+    it "has a customer" do
+    end
 
-  it "must be valid" do
-    value(rental).must_be :valid?
+    it "has a movie" do
+    end
+  end
+
+  describe "validations" do
+    it "requires a customer_id" do
+    end
+
+    it "requires a movie_id" do
+    end
+
+    it "requires a checkout date" do
+    end
+
+    it "requires a due date" do
+    end
   end
 end
