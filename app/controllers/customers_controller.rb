7f0c38cda619004c2cd9f0050feeda1777c8c430
Jansen <v.jansen.martin@gmail.com>
2019-05-14 15:01:05 -0700

    Add customers#index action; Add route; Add test

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,7 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+
+    render status: :ok, json: customers.as_json(only: [:name, :registered_at, :phone, :id])
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_index_url
-    value(response).must_be :success?
+  describe "index" do
+    it "can render without crashing" do
+      get customers_path
+
+      must_respond_with :ok
+    end
   end
+  # it "should get index" do
+  #   get customers_index_url
+  #   value(response).must_be :success?
+  # end
 
 end
