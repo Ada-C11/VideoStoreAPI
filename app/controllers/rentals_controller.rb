1464dbfce735d86f26bc7bec6e8770f5e86dc0c7
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-14 15:11:34 -0700

    fixed checkout

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,7 +2,7 @@ class RentalsController < ApplicationController
   def checkout
     rental = Rental.new(movie_id: params[:movie_id], customer_id: params[:customer_id], checkout_date: Date.today, due_date: (Date.today + 7))
     movie = Movie.find_by(id: rental.movie_id)
-    if !movie.available_inventory > 0
+    if movie.available_inventory <= 0
       render json: { ok: false, message: "Not enough copies in inventory" }, status: :bad_request
     else
       movie.available_inventory -= 1
