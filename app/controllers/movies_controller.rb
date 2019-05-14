f80525eac594d27c692287c5f47a34ff03b11e36
sjscotton <sj.scotton@gmail.com>
2019-05-14 13:32:36 -0700

    fixed test helper file bug, added movie#show and nominal test case

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+  def show
+    id = params[:id]
+    movie = Movie.find_by(id: id)
+    if movie
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date]), status: :ok
+    else
+      render json: { errors: ["The movie with id #{id} was not found"] }, status: :not_found
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "show" do
+    it "can get a movie by id" do
+      movie = movies.first
+      get movie_path(movie.id)
+      must_respond_with :success
+    end
+  end
 end
