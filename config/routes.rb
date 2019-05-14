6fb1679db884a2734066d6a975cf4d7feb0ea385
Angela <angela.ohh@gmail.com>
2019-05-14 12:48:00 -0700

    pseudocode routes for customers and movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get "/movies", to: movies#index
+  # get "/movies/:id", to: movies#show
+  # post "/movies", to: movies#create
+  # post "/movies/:id", to: movies#update
+  # get "/movies/:id, to: movies#destroy
+
+  # get "/customers", to: customers#index
+  # get "/customers/:id", to: customers#show
+  # post "/customers", to: customers#create
+  # post "/customers/:id", to: customers#update
+  # get "/customers/:id, to: customers#destroy
+
 end
