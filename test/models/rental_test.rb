f94b5a7c3c30e0fe324fde9cd9ef5ad1b62ba9ee
Shubha Rajan <shubharajan94@gmail.com>
2019-05-14 15:11:02 -0700

    added tests for availability

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -6,6 +6,21 @@ describe Rental do
   }
 
   it "must be valid" do
-    value(rental).must_be :valid?
+    expect(rental).must_be :valid?
+  end
+
+  describe "availability" do
+    let(:rental_two) {
+      Rental.new(
+        movie_id: movies(:other_test).id,
+        customer_id: customers(:shelley).id,
+        checkout: DateTime.now,
+        due: DateTime.now + 7.days,
+      )
+    }
+    it "won't be valid if movie is unavailable" do
+      rental_two.movie.inventory = 0
+      value(rental_two).wont_be :valid?
+    end
   end
 end
