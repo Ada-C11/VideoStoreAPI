e3e53a95bbb5a854d2cb34a3306130d37e738c2f
Jansen <v.jansen.martin@gmail.com>
2019-05-14 13:55:09 -0700

    Initialize rails project; Add movie model

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get /customers
+  # get /movies
+  # get /movies/:id  <=== id
+  # post /movies
+
+  # post /rentals/check-out <==== customer id, movie id
+  # post /rentals/check-in <==== customer id, movie id
+end
