3ff6512d58bcd6a9b542201660a85e526eb17500
Margaret Finnan <msfinnan@gmail.com>
2019-05-14 17:00:35 -0700

    rentals checkin action and controller tests complete

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,16 +1,22 @@
 class RentalsController < ApplicationController
-    def checkout
-        rental = Rental.new(customer_id: params[:customer_id], movie_id: params[:movie_id])
-        rental.checkout_date = Date.today
-        rental.due_date = Date.today + 7
-        if rental.save
-          render json: rental.as_json(only: [:customer_id, :movie_id, :checkout_date, :due_date, :id]), status: :ok
-        else
-          render json: {ok: false, errors: rental.errors.messages}, status: :bad_request
-        end
-      end
-    
-      def checkin
-        # rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:mov]])
-      end
+  def checkout
+    rental = Rental.new(customer_id: params[:customer_id], movie_id: params[:movie_id])
+    rental.checkout_date = Date.today
+    rental.due_date = Date.today + 7
+    if rental.save
+      render json: rental.as_json(only: [:customer_id, :movie_id, :checkout_date, :due_date, :id]), status: :ok
+    else
+      render json: {ok: false, errors: rental.errors.messages}, status: :bad_request
+    end
+  end
+
+  def checkin
+    rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:movie_id])
+    if rental
+      rental.destroy
+      render json: {ok: true, messages: "Rental checked in successfully"}, status: :success
+    else
+      render json: {ok: false, errors: "Rental not found"}, status: :bad_request
+    end
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 describe RentalsController do
-  
   describe "checkout" do
     let(:rental_data) {
       {
@@ -30,14 +29,42 @@ describe RentalsController do
       rental_data["customer_id"] = nil
 
       expect {
-      post checkout_path, params: rental_data
-    }.wont_change "Rental.count"
+        post checkout_path, params: rental_data
+      }.wont_change "Rental.count"
 
       body = JSON.parse(response.body)
 
       expect(body).must_be_kind_of Hash
       expect(body).must_include "errors"
       expect(body["errors"]).must_include "customer_id"
+      must_respond_with :bad_request
+    end
+  end
+
+  describe "checkin" do
+    let(:rental_data) {
+      {
+        customer_id: customers(:customer_one).id,
+        movie_id: movies(:movie_one).id,
+      }
+    }
+    it "destroys checkout when given valid data" do
+      expect {
+        post checkin_path, params: rental_data
+      }.must_change "Rental.count", -1
+    end
+
+    it "it returns an error if rental is not found" do
+      rental_data["customer_id"] = customers(:customer_two).id
+      expect {
+        post checkin_path, params: rental_data
+      }.wont_change "Rental.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "Rental not found"
       must_respond_with :bad_request
     end
   end
