4c018903b6ede46a3ba3f00d4509362918057023
Cyndi Lopez <cyndilopez6@gmail.com>
2019-05-15 13:04:55 -0700

    movie validation test for title, added movie create and show action tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -43,42 +43,83 @@ describe MoviesController do
   end
 
   describe "show" do
+    before do
+      @this_movie = movies(:GreenMile)
+    end
     it "can get a movie" do
-      this_movie = movies(:GreenMile)
-      get movie_path(this_movie)
+      get movie_path(@this_movie)
       must_respond_with :success
-      this_movie.title.must_equal "The Green Mile"
+      @this_movie.title.must_equal "The Green Mile"
     end
 
     it "returns json" do
-      # get movies_path
-      # expect(response.header["Content-Type"]).must_include "json"
+      get movie_path(@this_movie)
+      expect(response.header["Content-Type"]).must_include "json"
     end
 
-    it "returns an array" do
-      # get movies_path
-      # body = JSON.parse(response.body)
-      # body.must_be_kind_of Array
+    it "returns an Hash" do
+      get movie_path(@this_movie)
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
     end
 
     it "returns movie with the fields required to be included in the json" do
-      # keys = %w(id title overview release_date inventory)
-      # get movies_path
-      # body = JSON.parse(response.body)
-      # body.each do |movie|
-      #   movie.keys.must_equal keys
-      # end
+      keys = %w(id title overview release_date inventory)
+      get movie_path(@this_movie)
+      body = JSON.parse(response.body)
+      body.keys.must_equal keys
     end
 
     it "returns 404 if movie is not found" do
+      get movie_path(-1)
+      expect(response.header["Content-Type"]).must_include "json"
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      body.must_include "error"
     end
   end
 
   describe "create" do
+    let(:movie_params) {
+      {
+        title: "Stardust",
+        inventory: 10,
+        release_date: "20191010",
+        overview: "fantasy world star falls to earth and adventures ensue",
+      }
+    }
     it "makes a new movie" do
+      expect {
+        post movies_path, params: movie_params
+      }.must_change "Movie.count", +1
     end
 
     it "returns an error for invalid movie data" do
+      movie_params[:title] = nil
+      expect {
+        post movies_path, params: movie_params
+      }.wont_change "Movie.count"
+      must_respond_with :bad_request
+      body = JSON.parse(response.body)
+      body.must_include "errors"
+    end
+
+    it "returns json" do
+      post movies_path, params: movie_params
+      expect(response.header["Content-Type"]).must_include "json"
+    end
+
+    it "returns an Hash" do
+      post movies_path, params: movie_params
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+    end
+
+    it "returns movie with the fields required to be included in the json" do
+      keys = ["id"]
+      post movies_path, params: movie_params
+      body = JSON.parse(response.body)
+      body.keys.must_equal keys
     end
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { Movie.new(title: "Stardust") }
 
-it "must be valid" do
+  it "must be valid" do
     value(movie).must_be :valid?
+  end
+
+  it "returns false for invalid movie data" do
+    movie = Movie.new
+    movie.valid?.must_equal false
   end
 end
