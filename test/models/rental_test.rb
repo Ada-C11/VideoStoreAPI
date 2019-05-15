9fa3e1dddb17973b89b0c425d4cf6ee7b6a15dcb
sjscotton <sj.scotton@gmail.com>
2019-05-15 15:22:47 -0700

    added tests for rental validations

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,36 @@
 require "test_helper"
 
 describe Rental do
-  # let(:rental) { Rental.new }
+  describe "validations" do
+    let(:rental_info) {
+      {
+        movie_id: movies.first.id,
+        customer_id: customers.first.id,
+      }
+    }
+    it "can create a rental with good data" do
+      rental = Rental.new(rental_info)
+      result = rental.valid?
 
-  # it "must be valid" do
-  #   value(rental).must_be :valid?
-  # end
+      expect(result).must_equal true
+    end
+
+    it "wont be valid without a movie_id" do
+      rental_info[:movie_id] = nil
+      rental = Rental.new(rental_info)
+      result = rental.valid?
+
+      expect(result).must_equal false
+    end
+    it "wont be valid without a customer_id" do
+      rental_info[:customer_id] = nil
+      rental = Rental.new(rental_info)
+      result = rental.valid?
+
+      expect(result).must_equal false
+    end
+  end
+
+  describe "relationships" do
+  end
 end
