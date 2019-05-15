bf6075354a0565ec142a8363b2c2ef83d9d2125c
Nidhi <nidhixpatel@gmail.com>
2019-05-15 15:12:51 -0700

    added validation to customer

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,6 @@ class Movie < ApplicationRecord
   has_many :rentals
 
   validates :title, presence: true
-  # validates :overview, presence: true
-  # validates :release_date, presence: true
   validates :inventory, presence: true, numericality: true
 
   def available_inventory
