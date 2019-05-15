3d66436b4315527037ed17661dddfb3bd296d7ba
Nidhi <nidhixpatel@gmail.com>
2019-05-15 13:10:12 -0700

    change inventory

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,14 +1,17 @@
 class RentalsController < ApplicationController
 
   def check_out
-    customer  = Customer.find_by(id: rental_params[:customer_id])
-    movie  = Movie.find_by(id: rental_params[:movie_id])
+    customer  = Customer.find_by(id: params[:customer_id])
+    movie  = Movie.find_by(id: params[:movie_id])
     rental = Rental.new(check_out_date: Date.today, due_date: Date.today + 7.days, movie_id: movie.id, customer_id: customer.id )
     if rental.save
       render json: { id: rental.id }, status: :ok
+      movie.update(inventory: movie.inventory - 1)
     else
       render json: { errors: rental.errors.messages }, status: :bad_request
     end
+    
+   
   end
 
   private
