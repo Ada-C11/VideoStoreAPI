7d241646c373898b35086c4b4d9952bf9ee6dbe7
laneia <laneiamurray@gmail.com>
2019-05-15 14:21:02 -0700

    added available_inventory method to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,14 @@ class Movie < ApplicationRecord
 
   validates :title, presence: true
 
-  
+  def available_inventory
+    checkedout = 0
+
+    self.rentals.each do |rental|
+      if rental.status == "Checked Out"
+        checkedout += 1
+      end
+    end
+    return self.inventory - checkedout
+  end
 end
