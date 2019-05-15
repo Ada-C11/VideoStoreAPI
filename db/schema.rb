668aacfc075abe1d1091cafd18b83984c05133d3
Elise Pham <elise.pham88@gmail.com>
2019-05-15 14:09:17 -0700

    successfully added default value to checkedout counts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_193036) do
+ActiveRecord::Schema.define(version: 2019_05_15_210316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_193036) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "movies_checked_out_count"
+    t.integer "movies_checked_out_count", default: 0
   end
 
   create_table "movies", force: :cascade do |t|
