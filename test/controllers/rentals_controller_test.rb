fc3651254652c439d6b26736d0048a2293359a86
sjscotton <sj.scotton@gmail.com>
2019-05-15 12:38:42 -0700

    added checkout method

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "checkout" do
+    let(:rental_info) {
+      {
+        movie_id: movies.first.id,
+        customer_id: customers.first.id,
+      }
+    }
+    it "creates a new rental with valid data" do
+      expect {
+        post checkout_path, params: rental_info
+      }.must_change "Rental.count", 1
+    end
+
+    it "creates a rental with the correct checkout and due date" do
+    end
+
+    it "returns an error if given an invalid movie or customer id" do
+    end
+  end
 end
