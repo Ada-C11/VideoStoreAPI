3ef55b2d04dbf873000de0c6009520f70224ff09
Hana <hanalways@gmail.com>
2019-05-15 14:05:33 -0700

    creates an instance of rental test passing

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,13 +2,15 @@ class RentalsController < ApplicationController
   def check_out
     rental = Rental.new(rental_params)
 
+    rental.checkout_date = Date.current
     if rental.save
+      rental.due_date = rental.checkout_date + 7
       render status: :ok, json: {id: rental.id}
     else
       render status: :bad_request, json: {errors: rental.errors.messages}
     end
     # rental = Rental.new
-
+    
     # rental.checkout_date = Date.current
     # rental.due_date = rental.checkout_date + 7
 
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,16 +1,44 @@
 require "test_helper"
 
 describe RentalsController do
-  describe "create" do
-    let(:customer) { customers(:jessica) }
-    let(:movie) { movies(:harrypotter) }
-    let(:rental_data) {
-      {
-        customer_id: customer.id,
-        movie_id: movie.id,
-      }
+  let(:customer) { customers(:jessica) }
+  let(:movie) { movies(:harrypotter) }
+  let(:rental_data) {
+    {
+      customer_id: customer.id,
+      movie_id: movie.id,
     }
+  }
 
+  describe "check_out" do 
+    it "creates an instance of Rental" do 
+      expect {
+        post check_out_path, params: rental_data
+      }.must_change "Rental.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"      
+    end
+  end
+      # it "creates a new movie given valid data" do
+      #   expect {
+      #     post movies_path, params: movie_data
+      #   }.must_change "Movie.count", 1
+  
+      #   body = JSON.parse(response.body)
+      #   expect(body).must_be_kind_of Hash
+      #   expect(body).must_include "id"
+  
+      #   movie = Movie.find(body["id"].to_i)
+  
+      #   expect(movie.title).must_equal movie_data[:title]
+      #   expect(movie.overview).must_equal movie_data[:overview]
+      #   expect(movie.release_date).must_equal Date.parse(movie_data[:release_date])
+      #   expect(movie.inventory).must_equal movie_data[:inventory]
+  
+      #   must_respond_with :success
+      # end
     # it "returns an error if no params" do
     #   expect {
     #     post movies_path
@@ -23,24 +51,6 @@ describe RentalsController do
     #   must_respond_with :bad_request
     # end
 
-    # it "creates a new movie given valid data" do
-    #   expect {
-    #     post movies_path, params: movie_data
-    #   }.must_change "Movie.count", 1
-
-    #   body = JSON.parse(response.body)
-    #   expect(body).must_be_kind_of Hash
-    #   expect(body).must_include "id"
-
-    #   movie = Movie.find(body["id"].to_i)
-
-    #   expect(movie.title).must_equal movie_data[:title]
-    #   expect(movie.overview).must_equal movie_data[:overview]
-    #   expect(movie.release_date).must_equal Date.parse(movie_data[:release_date])
-    #   expect(movie.inventory).must_equal movie_data[:inventory]
-
-    #   must_respond_with :success
-    # end
 
     # it "returns an error if no title" do
     #   movie_data["title"] = nil
@@ -103,5 +113,4 @@ describe RentalsController do
     #   expect(body["errors"]).must_include "inventory"
     #   must_respond_with :bad_request
     # end
-  end
 end
