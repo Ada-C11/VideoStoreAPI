0d2b8054bbce1a4a46870d9b40f1dc05e32d0523
sjscotton <sj.scotton@gmail.com>
2019-05-15 15:29:07 -0700

    added relationship testing for rentals

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 describe Rental do
-  describe "validations" do
-    let(:rental_info) {
-      {
-        movie_id: movies.first.id,
-        customer_id: customers.first.id,
-      }
+  let(:rental_info) {
+    {
+      movie_id: movies.first.id,
+      customer_id: customers.first.id,
     }
+  }
+  describe "validations" do
     it "can create a rental with good data" do
       rental = Rental.new(rental_info)
       result = rental.valid?
@@ -32,5 +32,18 @@ describe Rental do
   end
 
   describe "relationships" do
+    before do
+      @rental = Rental.new(rental_info)
+    end
+    it "can access its movie through .movie" do
+      movie = @rental.movie
+      expect(movie).must_be_instance_of Movie
+      expect(movie.id).must_equal @rental.movie_id
+    end
+    it "can access its customer through .customer" do
+      customer = @rental.customer
+      expect(customer).must_be_instance_of Customer
+      expect(customer.id).must_equal @rental.customer_id
+    end
   end
 end
