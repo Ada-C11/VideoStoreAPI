3aff11da9306aa87666b745644081438206010d5
laneia <laneiamurray@gmail.com>
2019-05-15 14:54:41 -0700

    added available_inventory test one

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,18 +1,29 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { movies(:one) }
+  describe "validation tests" do
+    let(:movie) { movies(:one) }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+    it "must be valid" do
+      value(movie).must_be :valid?
+    end
+
+    it "requires a title" do
+      required_fields = [:title]
+      movie[:title] = nil
+
+      expect(movie.valid?).must_equal false
+
+      movie.reload
+    end
   end
 
-  it "requires a title" do
-    required_fields = [:title]
-    movie[:title] = nil
+  describe "available_inventory" do
+    it "can calculate available_inventory for movie with rentals" do
+      movie = movies(:one)
+      num_available = movie.available_inventory
 
-    expect(movie.valid?).must_equal false
-
-    movie.reload
+      expect(num_available).must_equal 4
+    end
   end
 end
