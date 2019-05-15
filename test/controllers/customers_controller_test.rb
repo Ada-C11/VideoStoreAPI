38a5c83f83b8f6edfcbfc308dcf2394b7d848371
Shubha Rajan <shubharajan94@gmail.com>
2019-05-15 13:53:51 -0700

    resolved merge conflict

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -29,11 +29,7 @@ describe CustomersController do
     end
 
     it "returns customers with exactly the required fields" do
-<<<<<<< HEAD
       keys = %w(address city movies_checked_out_count name phone postal_code registered_at state)
-=======
-      keys = %w(address city id name phone postal_code registered_at state)
->>>>>>> 864e9ad3f43c946e11c84b031389090cfec51ad8
       get customers_path
       body = JSON.parse(response.body)
       body.each do |customer|
