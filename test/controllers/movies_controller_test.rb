29b7a2819d817f65fae26009ca8b1ffbb5c56c60
Jessica <jessica.homet@gmail.com>
2019-05-15 13:43:58 -0700

    add more movie#create tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -94,7 +94,55 @@ describe MoviesController do
       }
     }
 
+    it "returns an error if no params" do
+      expect {
+        post movies_path
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      must_respond_with :bad_request
+    end
+
     it "creates a new movie given valid data" do
+      expect {
+        post movies_path, params: movie_data
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+
+      expect(movie.title).must_equal movie_data[:title]
+      expect(movie.overview).must_equal movie_data[:overview]
+      expect(movie.release_date).must_equal Date.parse(movie_data[:release_date])
+      expect(movie.inventory).must_equal movie_data[:inventory]
+
+      must_respond_with :success
+    end
+
+    it "returns an error if no title" do
+      movie_data["title"] = nil
+
+      expect {
+        post movies_path, params: movie_data
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
+    end
+
+    it "creates a new movie if no overview" do
+      movie_data["overview"] = nil
+
       expect {
         post movies_path, params: movie_data
       }.must_change "Movie.count", 1
@@ -109,8 +157,8 @@ describe MoviesController do
       must_respond_with :success
     end
 
-    it "returns an error for invalid movie data" do
-      movie_data["title"] = nil
+    it "returns an error if no release date" do
+      movie_data["release_date"] = nil
 
       expect {
         post movies_path, params: movie_data
@@ -120,7 +168,22 @@ describe MoviesController do
 
       expect(body).must_be_kind_of Hash
       expect(body).must_include "errors"
-      expect(body["errors"]).must_include "title"
+      expect(body["errors"]).must_include "release_date"
+      must_respond_with :bad_request
+    end
+
+    it "returns an error if no inventory" do
+      movie_data["inventory"] = nil
+
+      expect {
+        post movies_path, params: movie_data
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "inventory"
       must_respond_with :bad_request
     end
   end
