a8d205d556147bfcf7ae1836a8ae502f7118c1b8
Erica Norris <norrise120@gmail.com>
2019-05-15 13:18:57 -0700

    migrations to fix rentals schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_224413) do
+ActiveRecord::Schema.define(version: 2019_05_15_201728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,13 +38,12 @@ ActiveRecord::Schema.define(version: 2019_05_14_224413) do
 
   create_table "rentals", force: :cascade do |t|
     t.date "checkout_date"
-    t.string "due_date"
-    t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "movie_id"
     t.bigint "customer_id"
     t.boolean "currently_checked_out"
+    t.date "due_date"
     t.index ["customer_id"], name: "index_rentals_on_customer_id"
     t.index ["movie_id"], name: "index_rentals_on_movie_id"
   end
