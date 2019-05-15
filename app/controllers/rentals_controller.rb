b936da33d859a36aa99e965b5a125974b2dedeb2
Jansen <v.jansen.martin@gmail.com>
2019-05-15 15:53:38 -0700

    Add check_out and check_in actions to rentals_controller

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,28 @@
+class RentalsController < ApplicationController
+  def check_out
+    movie = Movie.find_by(id: params[:movie][:id])
+    customer = Customer.find_by(id: params[:customer][:id])
+    rental = Rental.new(check_out: DateTime.now, movie_id: movie.id, customer_id: customer.id, due_date: DateTime.now + 7)
+
+    if rental.save
+      # Decrease Inventory by 1 (via helper method)
+    else
+      # Error message
+    end
+  end
+
+  def check_in
+    movie = Movie.find_by(id: params[:movie][:id])
+    customer = Customer.find_by(id: params[:customer][:id])
+    rental = Rental.where(customer_id: customer.id, movie_id: movie.id)
+
+    # rental.update(check_in: DateTime.now )
+
+    #if successful
+    # Increase Inventory by 1 (via helper method)
+    #else
+    # Error message
+    #end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rentals/check_out'
+  get 'rentals/check_in'
   resources :movies, only: [:index, :show, :create]
   resources :customers, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
