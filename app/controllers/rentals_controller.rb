f78e1f728387cbbb6e71fa96e3832dbe1e06f04b
Nidhi <nidhixpatel@gmail.com>
2019-05-15 13:14:09 -0700

    added no available copies for checkout

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,15 +3,17 @@ class RentalsController < ApplicationController
   def check_out
     customer  = Customer.find_by(id: params[:customer_id])
     movie  = Movie.find_by(id: params[:movie_id])
-    rental = Rental.new(check_out_date: Date.today, due_date: Date.today + 7.days, movie_id: movie.id, customer_id: customer.id )
-    if rental.save
-      render json: { id: rental.id }, status: :ok
-      movie.update(inventory: movie.inventory - 1)
+    if movie.inventory > 0
+      rental = Rental.new(check_out_date: Date.today, due_date: Date.today + 7.days, movie_id: movie.id, customer_id: customer.id )
+      if rental.save
+        render json: { id: rental.id, inventory: movie.inventory }, status: :ok
+        movie.update(inventory: movie.inventory - 1)
+      else
+        render json: { errors: rental.errors.messages }, status: :bad_request
+      end
     else
-      render json: { errors: rental.errors.messages }, status: :bad_request
+      render json: { errors: "No available copies for checkout" }, status: :bad_request 
     end
-    
-   
   end
 
   private
