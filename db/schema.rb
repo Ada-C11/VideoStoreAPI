f3438a98665857ecfe87bb9d1456dd841995b14c
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-15 13:39:32 -0700

    Migration: Add due_date column to Rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_220129) do
+ActiveRecord::Schema.define(version: 2019_05_15_203145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_220129) do
     t.datetime "updated_at", null: false
     t.bigint "customer_id"
     t.bigint "movie_id"
+    t.datetime "due_date"
     t.index ["customer_id"], name: "index_rentals_on_customer_id"
     t.index ["movie_id"], name: "index_rentals_on_movie_id"
   end
