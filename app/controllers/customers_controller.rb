c0e0818677f8a77c27e8fa448e96eeb6f7be5e0f
Kate Sandler <kate@katekatekate.net>
2019-05-15 15:37:53 -0700

    Fixed a test

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,6 +8,6 @@ class CustomersController < ApplicationController
   private
 
   def format_json(customer_data)
-    return customer_data.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
+    return customer_data.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count])
   end
 end
