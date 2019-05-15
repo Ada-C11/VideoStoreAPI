32966710a53e46e3b620f25169b8e89ad1482000
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-15 12:57:50 -0700

    added check_in action to rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,17 @@
 class RentalsController < ApplicationController
+  def check_in
+    rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:movie_id])
+    # check_in: nil
+
+    if rental
+    else
+      render json: { errors: ["Rental not found"] }
+    end
+  end
+
+  private
+
+  def rental_params
+    params.permit(:customer_id, :movie_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
+
+  post "/rentals/check_in", to: "rentals#check_in", as: "check_in"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
