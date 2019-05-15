8d2339436876ecc98e8b123d3c086d4109da26f9
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-05-15 14:57:04 -0700

    Created a new migration for rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_213810) do
+ActiveRecord::Schema.define(version: 2019_05_15_214530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 2019_05_15_213810) do
     t.string "overview"
     t.date "release_date"
     t.integer "inventory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "customer_id"
+    t.datetime "checkout_date"
+    t.datetime "due_date"
+    t.boolean "currently_checked_out"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
