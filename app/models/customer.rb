581df1a5b8b2a4f7ad16e5b1966fbedb24ccd8e2
Shirley Chu <shirley.cchu@gmail.com>
2019-05-15 13:06:04 -0700

    customer model and controller tests refactored

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,5 @@
 class Customer < ApplicationRecord
   validates :name, presence: true
-  validates :registered_at, presence: true
-  validates :postal_code, presence: true
   validates :phone, presence: true
 
   def movies_checked_out_count
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -9,19 +9,13 @@ describe Customer do
       expect(customer.valid?).must_equal true
     end
 
-    it "requires name, phone number, postal code, and registered at" do
-      required_fields = [:name, :phone, :postal_code, :registered_at]
+    it "requires name and phone number" do
+      required_fields = [:name, :phone]
       required_fields.each do |field|
         customer[field] = nil
         expect(customer.valid?).must_equal false
         customer.reload
       end
-    end
-
-    it "registered at must be date" do
-      customer.registered_at = "hi!"
-      expect(customer.valid?).must_equal false
-      customer.reload
     end
   end
 
