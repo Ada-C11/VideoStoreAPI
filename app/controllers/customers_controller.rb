a910a607c30e31dacac8252b0265bee14d114952
amyesh <amyesh08@gmail.com>
2019-05-15 11:37:46 -0700

    removed show and create methods from customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,34 +4,4 @@ class CustomersController < ApplicationController
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
            status: :ok
   end
-
-  def show
-    customer = Customer.find_by(id: params[:id])
-
-    if !customer.nil?
-      render json: customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]),
-             status: :ok
-    else
-      render json: {ok: false, errors: {customer: ["customer not found"]}},
-             status: :not_found
-    end
-  end
-
-  def create
-    customer = Customer.new(customer_params)
-
-    if customer.save
-      render json: customer.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count, :id]),
-             status: :ok
-    else
-      render json: {ok: false, errors: customer.errors.messages},
-             status: :bad_request
-    end
-  end
-
-  private
-
-  def customer_params
-    params.require(:customer).permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count)
-  end
 end
