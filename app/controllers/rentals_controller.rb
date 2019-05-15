c7b3548c3699e68aade44b5f9dc517fb5e922697
laneia <laneiamurray@gmail.com>
2019-05-15 12:17:06 -0700

    added rental_params

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,13 @@
 class RentalsController < ApplicationController
+  def checkout
+  end
+
+  def checkin
+  end
+
+  private
+
+  def rental_params
+    params.permit(:movie_id, :customer_id)
+  end
 end
