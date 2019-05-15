b9c5c20c2538c833d877bcb909eef3106a374306
Shubha Rajan <shubharajan94@gmail.com>
2019-05-15 13:54:22 -0700

    resolved merge conflict

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,7 @@
 class CustomersController < ApplicationController
   def index
-<<<<<<< HEAD
     customers = Customer.all.map { |customer| customer.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]).merge({ "movies_checked_out_count": customer.checked_out_count }) }
     render status: :ok, json: customers.as_json
-=======
-    customers = Customer.all
-    render status: :ok, json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
->>>>>>> 864e9ad3f43c946e11c84b031389090cfec51ad8
   end
 
   private
