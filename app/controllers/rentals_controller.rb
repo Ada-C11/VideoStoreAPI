efe362178de1d294ac0ae217f8c5c60390beda18
laneia <laneiamurray@gmail.com>
2019-05-15 12:47:53 -0700

    added checkout rentals controller method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,8 +1,20 @@
 class RentalsController < ApplicationController
   def checkout
-  end
+    rental = Rental.new(rental_params)
 
-  def checkin
+    rental.checkout = Date.today
+    rental.due_date = rental.checkout + 7
+    rental.status = "Checked Out"
+
+    if rental.available?(rental_params[:movie_id])
+      if rental.save
+        render json: rental.as_json(except: [:created_at, :updated_at]), status: :ok
+      else
+        render json: {errors: rental.errors.messages}, status: :bad_request
+      end
+    else
+      render json: {errors: "Movie unavailable."}, status: :bad_request
+    end
   end
 
   private
