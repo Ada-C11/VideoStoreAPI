efcea08ea2b400484d37920d8675b0b3fc434d3b
amyesh <amyesh08@gmail.com>
2019-05-15 15:58:55 -0700

    added tests for rental checkin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show, :create]
-  resources :customers, only: [:index, :show, :create]
+  resources :customers, only: [:index]
+
+  post "/rentals/checkin", to: "rentals#checkin", as: "checkin"
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,19 +1,42 @@
 require "test_helper"
 
 describe RentalsController do
-  it "should get index" do
-    get rentals_index_url
-    value(response).must_be :success?
-  end
+  describe "Checkin" do
+    let(:movie) { movies(:one) }
+    let(:customer) { customers(:one) }
+    let(:rental_data) {
+      {
+        movie_id: movie.id,
+        customer_id: customer.id,
+      }
+    }
+    it "should pass given valid data" do
+      post checkin_path, params: {rental: rental_data}
 
-  it "should get show" do
-    get rentals_show_url
-    value(response).must_be :success?
-  end
+      value(response).must_be :success?
+      expect(response.header["Content-Type"]).must_include "json"
 
-  it "should get create" do
-    get rentals_create_url
-    value(response).must_be :success?
-  end
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
 
+      rental = Rental.find(body["id"].to_i)
+
+      expect(rental.customer_id).must_equal rental_data[:customer.id]
+      expect(rental.movie_id).must_equal rental_data[:movie.id]
+    end
+
+    it "throws an error and returns BAD REQUEST if given invalid data" do
+      rental_data["customer_id"] = -1
+
+      post checkin_path, params: {rental: rental_data}
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "customer_id"
+      must_respond_with :bad_request
+    end
+  end
 end
