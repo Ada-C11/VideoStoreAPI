62ecbb2ed6e3df610dd1bcc892d2b3fc39e3037f
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-15 13:19:20 -0700

    added some logic for inventory count which will need to be pulled out into the model

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,9 +2,12 @@ class RentalsController < ApplicationController
   def check_in
     rental = Rental.find_by(customer_id: params[:customer_id], movie_id: params[:movie_id], check_in_date: nil)
     customer = Customer.find_by(id: params[:customer_id])
+    movie = Movie.find_by(id: params[:movie_id])
 
     if rental
       rental.check_in_date = Date.current
+      movie.inventory += 1
+      movie.save
       # adjust customer check out count?
       render json: rental.as_json(only: [:id, :check_in_date]), status: :ok
     else
