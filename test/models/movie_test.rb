42e071cddef19c875c9eb38c47b656ee314ba080
Angela <angela.ohh@gmail.com>
2019-05-15 13:13:06 -0700

    added customer model and movie model test

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,4 +1,10 @@
 require "test_helper"
 
 describe Movie do
+  describe "custom methods" do
+    let(:movie) { Movie.first }
+    it "will return a value for movies_checked out" do
+      expect(movie.available_inventory).must_equal movie.inventory
+    end
+  end
 end
