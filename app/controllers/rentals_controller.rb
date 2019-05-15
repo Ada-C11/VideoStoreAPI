cb295c49b433ef9ebfe756187a7c4111d8d042fd
laneia <laneiamurray@gmail.com>
2019-05-15 12:49:40 -0700

    added checkin rentals method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -17,6 +17,20 @@ class RentalsController < ApplicationController
     end
   end
 
+  def checkin
+    rental = Rental.find_by(movie_id: rental_params[:movie_id], customer_id: rental_params[:customer_id], status: "Checked Out")
+    if rental
+      rental.status = "Checked In"
+      if rental.save
+        render json: rental.as_json(except: [:created_at, :updated_at]), status: :ok
+      else
+        render json: {errors: rental.errors.messages}, status: :bad_request
+      end
+    else
+      render json: {errors: "Rental not located, please check information."}, status: :not_found
+    end
+  end
+
   private
 
   def rental_params
