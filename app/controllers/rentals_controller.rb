f512e8c86406131abb5c3f496c69e93f0028c003
Nidhi <nidhixpatel@gmail.com>
2019-05-15 13:58:03 -0700

    added tests for checkout method in rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,18 +1,28 @@
 class RentalsController < ApplicationController
 
   def check_out
+    error = "No errors found"
+
     customer  = Customer.find_by(id: params[:customer_id])
+    error = "Customer not found" if customer == nil
+
     movie  = Movie.find_by(id: params[:movie_id])
-    if movie.inventory > 0
-      rental = Rental.new(check_out_date: Date.today, due_date: Date.today + 7.days, movie_id: movie.id, customer_id: customer.id )
+    error = "Movie not found" if movie == nil
+    error = "No available copies for checkout" if movie && movie.inventory < 1
+
+    if movie && movie.inventory > 0 && customer
+      rental = Rental.new(check_out_date: Date.today, 
+                          due_date: Date.today + 7.days, 
+                          movie_id: movie.id, 
+                          customer_id: customer.id )
       if rental.save
         render json: { id: rental.id, inventory: movie.inventory }, status: :ok
         movie.update(inventory: movie.inventory - 1)
       else
-        render json: { errors: rental.errors.messages }, status: :bad_request
+        error = rental.errors.messages
       end
     else
-      render json: { errors: "No available copies for checkout" }, status: :bad_request 
+      render json: { errors: error }, status: :bad_request 
     end
   end
 
