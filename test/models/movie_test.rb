8249030caad39bec4bd3acb4d0af0f11038c9673
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-15 15:54:39 -0700

    validation tests for movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -25,6 +25,51 @@ describe Movie do
     end
   end
 
+  describe "validations" do
+    it "must have a title" do
+      movie.title = nil
+
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :title
+      expect(movie.errors.messages[:title]).must_equal ["can't be blank"]
+    end
+
+    it "must have an inventory value" do
+      movie.inventory = nil
+
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :inventory
+      expect(movie.errors.messages[:inventory]).must_include "can't be blank"
+    end
+
+    it "iventory amount must be an integer" do
+      movie.inventory = "cat"
+
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :inventory
+      expect(movie.errors.messages[:inventory]).must_equal ["is not a number"]
+    end
+
+    it "must have an inventory amount greater than -1" do
+      inventory = [0, 1, 10, 100, 4000]
+      inventory.each do |amount|
+        movie.inventory = amount
+        expect(movie.valid?).must_equal true
+      end
+
+      inventory = [-1, -5, -10, -100]
+      inventory.each do |amount|
+        movie.inventory = amount
+
+        expect(movie.valid?).must_equal false
+        expect(movie.errors.messages).must_include :inventory
+        expect(movie.errors.messages[:inventory]).must_equal ["must be greater than -1"]
+      end
+    end
+  end
+
   describe "custom methods" do
+    describe "available_inventory" do
+    end
   end
 end
