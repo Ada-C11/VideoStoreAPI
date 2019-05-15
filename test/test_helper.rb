17a299c565c794caa39b46439af32838992440dd
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-15 15:17:55 -0700

    updated testhelper to ignore certain files for simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
-require "minitest/reporters"  # for Colorized output
+require "minitest/reporters"
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/app/mailers/"
+  add_filter "/app/channels/"
+  add_filter "app/jobs/"
+end
+
+# for Colorized output
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
