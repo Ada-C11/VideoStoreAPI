a173b16709e630fd439ae173eea2dc9d8cbcd91c
laneia <laneiamurray@gmail.com>
2019-05-16 09:25:09 -0700

    added rental validations and tests, relevant test fixtures

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,9 +2,7 @@ class Rental < ApplicationRecord
   belongs_to :movie
   belongs_to :customer
 
-  validates :customer_id, presence: true
-  validates :movie_id, presence: true
-
+  validates :customer_id, :movie_id, :due_date, :checkout, :status, presence: true
 
   def available?(movie_id)
     movie = Movie.find_by(id: movie_id)
