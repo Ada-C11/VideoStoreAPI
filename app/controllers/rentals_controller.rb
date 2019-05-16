7d88f98f138dd37491f0655a7b9c6bfa888ccf34
amyesh <amyesh08@gmail.com>
2019-05-15 22:05:59 -0700

    wrote rental checkin method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,10 +1,11 @@
 class RentalsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def create
+  def checkin
+    rental = Rental.find_by(customer_id: rentals_params[:customer_id], movie_id: rentals_params[:movie_id])
+    if rental
+      render json: {id: rental.id, movie_id: rental.movie.id, customer_id: rental.customer.id}, status: :ok
+    else
+      render json: {errors: {rental: ["Rental not found for customer #{rental_params[:customer_id]}, and movie #{rental_params[:movie_id]}"]}},
+             status: :not_found
+    end
   end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -21,22 +21,23 @@ describe RentalsController do
       expect(body).must_include "id"
 
       rental = Rental.find(body["id"].to_i)
-
-      expect(rental.customer_id).must_equal rental_data[:customer.id]
-      expect(rental.movie_id).must_equal rental_data[:movie.id]
+      expect(rental.customer_id).must_equal rental_data[:customer_id]
+      expect(rental.movie_id).must_equal rental_data[:movie_id]
     end
 
     it "throws an error and returns BAD REQUEST if given invalid data" do
-      rental_data["customer_id"] = -1
-
-      post checkin_path, params: {rental: rental_data}
-
+      bad_data = {
+        rental: {
+          movie_id: movie.id,
+          customer_id: -1,
+        },
+      }
+      post checkin_path, params: {rental: bad_data}
       body = JSON.parse(response.body)
-
       expect(body).must_be_kind_of Hash
       expect(body).must_include "errors"
-      expect(body["errors"]).must_include "customer_id"
-      must_respond_with :bad_request
+      expect(body["errors"]).must_include "rental"
+      must_respond_with :not_found
     end
   end
 end
