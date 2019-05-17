1b8e72836c9cb9ccdcf7dfcd8e973bfebb5f26e1
Ariana Bray <aribray@gmail.com>
2019-05-17 13:06:54 -0700

    added rental model validation and relationship tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -5,8 +5,37 @@ require 'pry'
 
 describe Rental do
   let(:rental) { Rental.new(customer_id: customers(:joe).id, movie_id: movies(:one).id) }
+  let(:customer) { customers(:joe) }
 
-  it 'must be valid' do
-    value(rental).must_be :valid?
+  describe 'validations' do
+    it 'must be valid' do
+      value(rental).must_be :valid?
+    end
+
+    it 'must have a customer' do
+      rental.customer_id = nil
+      rental.save
+      expect(rental.valid?).must_equal false
+      expect(rental.errors).must_include 'customer'
+    end
+
+    it "must have a movie" do
+      rental.movie_id = nil
+      rental.save
+      expect(rental.valid?).must_equal false
+      expect(rental.errors).must_include "movie"
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to a customer" do
+      rental.save
+      expect(rental).must_respond_to :customer
+    end
+
+    it "belongs to a movie" do
+      rental.save
+      expect(rental).must_respond_to :movie
+    end
   end
 end
