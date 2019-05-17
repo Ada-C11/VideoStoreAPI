e0f7f029f38b0422ec66b0ef1f403aa8c3043cf9
Kelly Downes <kedownes@gmail.com>
2019-05-17 14:06:45 -0700

    Clean up merge

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,11 +4,11 @@ class RentalsController < ApplicationController
   def checkout
     movie = Movie.find_by(id: rental_params[:movie_id])
     if !movie
-      render json: { "errors": { "movie": ["Movie not found."] } }, status: :bad_request
+      render json: {"errors": {"movie": ["Movie not found."]}}, status: :bad_request
     else
       customer = Customer.find_by(id: rental_params[:customer_id])
       if !customer
-        render json: { "errors": { "customer": ["Customer not found."] } }, status: :bad_request
+        render json: {"errors": {"customer": ["Customer not found."]}}, status: :bad_request
       else
         checkout_date = Date.today
         due_date = checkout_date + 7
@@ -19,15 +19,15 @@ class RentalsController < ApplicationController
             customer.movies_checked_out_count += 1
             movie.available_inventory -= 1
             if !rental.save
-              render json: { "errors": { "movie": rental.movie.errors.messages } }, status: :bad_request
+              render json: {"errors": {"movie": rental.movie.errors.messages}}, status: :bad_request
             else
               render json: rental.as_json(only: [:id]), status: :ok
             end
           else
-            render json: { "errors": { "movie": rental.errors.messages } }, status: :bad_request
+            render json: {"errors": {"movie": rental.errors.messages}}, status: :bad_request
           end
         else
-          render json: { "errors": { "movie": ["#{movie.title} is out of stock"] } }, status: :bad_request
+          render json: {"errors": {"movie": ["#{movie.title} is out of stock"]}}, status: :bad_request
         end
       end
     end
@@ -38,13 +38,6 @@ class RentalsController < ApplicationController
     movie = Movie.find_by(id: rental_params[:movie_id])
     rental = Rental.find_by(customer_id: rental_params[:customer_id], movie_id: rental_params[:movie_id])
 
-<<<<<<< HEAD
-    customer.movies_checked_out_count -= 1
-    movie.available_inventory += 1
-    binding.pry
-    if rental.save
-      render json: { id: rental.id }, status: :ok
-=======
     if rental
       rental.movie.available_inventory += 1
       rental.movie.save
@@ -53,10 +46,9 @@ class RentalsController < ApplicationController
       render json: {id: rental.id}, status: :ok
     else
       render json: {errors: ["Rental could not be located"]}, status: :not_found
->>>>>>> 058d121aac852cb889b78f2c99af0bcf21344e4c
     end
   end
-  
+
   private
 
   def rental_params
