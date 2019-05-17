11d465959245c3580ebc653e7e2db034a800a259
Kelly Downes <kedownes@gmail.com>
2019-05-17 12:22:08 -0700

    Add movie controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -39,7 +39,60 @@ describe MoviesController do
   describe "show" do
     it "can get a movie" do
       get movie_path(movies(:one).id)
+      expect(response.header["Content-Type"]).must_include "json"
       must_respond_with :success
+    end
+
+    it "still returns json if the movie doesn't exist" do
+      get movie_path(-1)
+      expect(response.header["Content-Type"]).must_include "json"
+    end
+
+    it "returns 404 if movie not found" do
+      get movie_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "Test Movie",
+        inventory: 1,
+        available_inventory: 1,
+        overview: "test",
+        release_date: 2018 - 10 - 20,
+      }
+    }
+
+    it "creates a new movie given valid data" do
+      expect {
+        post movies_path, params: {movie: movie_data}
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+
+      expect(movie.title).must_equal movie_data[:title]
+      must_respond_with :success
+    end
+
+    it "returns an error for invalid movie data" do
+      movie_data["title"] = nil
+
+      expect {
+        post movies_path, params: {movie: movie_data}
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
     end
   end
 end
