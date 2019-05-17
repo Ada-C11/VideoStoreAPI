3b0f7fe777bf8da3f728af947d20744097843889
Stephanie Marchante <smarcha04@gmail.com>
2019-05-17 11:10:14 -0700

    adding available inventory column to schema

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,5 +3,4 @@ class CustomersController < ApplicationController
     customers = Customer.all
     render status: :ok, json: customers.as_json(only: [:name, :id, :registered_at, :postal_code, :phone, :movies_checked_out_count])
   end
-
 end
